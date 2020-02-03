025ac81483ad4425a9f39ea2e38def946c90b861
Mateusz <jaworski92@gmail.com>
2020-02-03 22:27:34 +0100

    Debugger, Break Point VS

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -8,15 +8,15 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            string name = args[0];
-            Console.WriteLine(name);
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Witaj " + name);
+            //Console.WriteLine(name);
+            //Console.WriteLine("Hello World");
+            //Console.WriteLine("Witaj " + name);
 
-            //foreach (var item in args)
-            //{
-            //    Console.WriteLine("Witaj " + item);
-            //}
+            foreach (var item in args)
+            {
+                Console.WriteLine("Witaj " + item);
+            }
         }
     }
 }
+
