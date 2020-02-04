ad31ae8ab47c7115d36d04d7d9028272c20c9880
Mateusz <jaworski92@gmail.com>
2020-02-04 23:44:57 +0100

    StudentDiary - 1step (core methods)

---
+++ StrudentDiary/Diary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        // Stany (zmienne - pola)
+        List<float> ratings;
+
+        //Zachowania
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalcualteAvarage()
+        {
+            float sum = 0, avg = 0;
+
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            avg = sum / ratings.Count();
+
+            return avg;
+        }
+
+        public float GetMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float GetMinRating()
+        {
+            return ratings.Min();
+        }
+
+    }
+}
+
+
+
+
