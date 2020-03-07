b73a7d873817b8e54e0f1478840040220a5a6e59
Mateusz <jaworski92@gmail.com>
2020-03-07 22:13:31 +0100

    Members, params

---
+++ Members/Diary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Members
+{
+    public class Diary
+    {
+        public Diary()
+        {
+            ratings = new List<float>();
+        }
+
+        // Stany (zmienne - pola)
+        private List<float> ratings;
+
+        public string Name;
+        public DiaryStatistics com;
+
+        //Zachowania
+        /// <summary>
+        /// Adding ratings
+        /// </summary>
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+        /// <summary>
+        /// Statistics: avg, max, min ratings
+        /// </summary>
+        /// <returns></returns>
+        public DiaryStatistics ComputeStatistics()
+        {
+            DiaryStatistics stats = new DiaryStatistics();
+
+            float sum = 0f;
+
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            stats.AvgarageGrade = sum / ratings.Count();
+            stats.MaxGrade = ratings.Max();
+            stats.MinGrade = ratings.Min();
+
+            return stats;
+        }
+
+    }
+}
+
+
+
+
---
+++ Members/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+namespace Members
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            diary.AddRating(9.5f);
+            diary.AddRating(5.4f);
+            diary.AddRating(8.4f);
+
+            DiaryStatistics stats = diary.ComputeStatistics();
+            WriteResult("Srednia", stats.AvgarageGrade, 3,5,7,8,9);
+            WriteResult("Maksymalna ocena", (int)stats.MaxGrade);
+            WriteResult("Minimalna ocena", (long)stats.MinGrade);
+            WriteResult("Minimalna ocena", stats.MinGrade, 1);
+        }
+
+        static void WriteResult(string description, params float[] result)
+        {
+            Console.WriteLine(description + ": " + result[0] + "   ----  " + result[4]);
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void WriteResult(string description, long result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void WriteResult(string description, float result, long test)
+        {
+            Console.WriteLine(description + ": " + result + ", " + test);
+            Console.WriteLine("{0}: {1:C1} {2}: {3}: {4}: {5}:", description, result, 2,3,4,5);
+            Console.WriteLine($"{description}: {result:C1} {2}: {3}: {4}: {5}:"); ///added in C# v.6
+        }
+    }
+}
