500f7fd7f37f9af44cfe955da46576c54b03aca1
Mateusz <jaworski92@gmail.com>
2020-03-20 18:18:52 +0100

    Loops - jumping; region

--- FlowControl2/Program.cs
+++ FlowControl2/Program.cs
@@ -12,42 +12,107 @@ namespace FlowControl
         static void Main(string[] args)
         {
             //InstructionIf();
-
             //Switch();
+            //ForLoop();
+            //int i = WhileLoop();
+            //int i = DoWhileLoop(i);
+            //ForEachLoop();
 
-            //for (int x = 1; x <= 6; x++)
+            int[] ages = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //foreach (var age in ages)
             //{
-            //    for (int y = 1; y <= 6; y++)
+            //    if (age == 2)
             //    {
-            //        m = x * y;
-            //        Console.WriteLine("Wartość x: {0} oraz wartosc y: {1}. Mnoznik: {2}", x, y, m);
+            //        Console.WriteLine("Age = {0}", age);
+            //        continue;
+            //        Console.WriteLine("Hello");
             //    }
 
+            //    if (age == 4)
+            //    {
+            //        Console.WriteLine("Age = {0}", age);
+            //        break;
+            //        Console.WriteLine("Hello");
+            //    }
+            //}            
+
+            //foreach (var age in ages)
+            //{
+            //    if (age == 2)
+            //    {
+            //        Console.WriteLine("Age = {0}", age);
+            //        goto skip;
+            //    }
             //}
 
-            //int i = 1;
+            //skip:
+            //Console.WriteLine("Hello");
 
-            //while (i < 3) 
-            //{
-            //    Console.WriteLine("Wartosc i: {0}", i);
-            //    i++;
-            //}
-
-            //do
-            //{
-            //    Console.WriteLine("Wartosc i: {0}", i);
-            //    i++;
-            //} while (i > 3);
+            CheckAge(ages);
 
             //Console.ReadKey();
 
+        }
+
+        #region private methods
+        private static void CheckAge(int[] ages)
+        {
+            foreach (var age in ages)
+            {
+                if (age == 3)
+                {
+                    Console.WriteLine("Age = {0}", age);
+                    return;
+                }
+            }
+        }
+
+        private static void ForLoop()
+        {
+            for (int x = 1; x <= 6; x++)
+            {
+                for (int y = 1; y <= 6; y++)
+                {
+                    m = x * y;
+                    Console.WriteLine("Wartość x: {0} oraz wartosc y: {1}. Mnoznik: {2}", x, y, m);
+                }
+
+            }
+        }
+
+        private static void ForEachLoop()
+        {
             int[] tab = { 1, 2, 3, 4, 5, 6, 25, 423 };
 
             foreach (var item in tab)
             {
                 Console.WriteLine("Wartosc x z tablicy to: {0}", item);
             }
-                                 
+        }
+
+        private static int DoWhileLoop(int i)
+        {
+            
+            do
+            {
+                Console.WriteLine("Wartosc i: {0}", i);
+                i++;
+            } while (i > 3);
+            return i;
+        }
+
+        private static int WhileLoop()
+        {
+            int i = 1;
+
+            while (i < 3)
+            {
+                Console.WriteLine("Wartosc i: {0}", i);
+                i++;
+            }
+
+            return i;
         }
 
         private static void Switch()
@@ -136,5 +201,6 @@ namespace FlowControl
         {
             Console.WriteLine("Masz 10 lub mniej lat Twój wiek to: " + age);
         }
+        #endregion
     }
 }
